1e99230c6d11817b3aa87dece3f17f1927cae782
TruthRAS <109167456+TruthRAS@users.noreply.github.com>
2025-08-08 15:57:15 +0100

    Update1 pysphere-assignment

--- pysphere-assignment-01.py
+++ pysphere-assignment-01.py
@@ -1,1 +1,7 @@
-print("assignment ")
+print("*** Velocity Calculator ***")
+I=input("Enter Current 'I' in amperes : ")
+I_int=int(I)
+R=input("Enter Resistance in ohms 'R': ")
+R_int=int(R)
+V=I_int*R_int
+print("Velocity of I of "+ I + " resistance of " +R + " is: "+str(V))
